ed90491f0296ce93511fca1ee8a312c144d0d4a8
Loan nguyen <nguyenloan990811@gmail.com>
2023-05-25 22:47:04 +0700

    Have done all exercises

---
+++ 4-practice/main.js
@@ -0,0 +1,6 @@
+let inputWidth = prompt('Enter the width');
+let inputHeight = prompt('Enter the height');
+let width = Number(inputWidth);
+let height = Number(inputHeight);
+let area = width * height;
+document.write('The area is : ' + area);
